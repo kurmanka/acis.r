e8b8300fc6e09ffbed541c4d38ed726bce01fb7f
Ivan Kurmanov <duraley@gmail.com>
2006-06-27 16:02:34 -0400

    [project @ 2006-06-27 20:02:34 by ivan]
    added build_citations_index and cit_document_similarity and tests for it\; also changed normalization
    
    darcs-hash:20060627200234-a15f8-dd93a7e55ac86697add08d6020bba930bd63bdbe.gz

--- lib/ACIS/Citations/Utils.pm
+++ lib/ACIS/Citations/Utils.pm
@@ -2,15 +2,20 @@ package ACIS::Citations::Utils;
 
 use strict;
 use warnings;
+use Carp::Assert;
 
 use Exporter;
 
 use base qw( Exporter );
 use vars qw( @EXPORT_OK );
 
-@EXPORT_OK = qw( normalize_string get_document_authors get_author_sid );
+@EXPORT_OK = qw( normalize_string build_citations_index
+                 get_document_authors get_author_sid );
+
 
 use Unicode::Normalize;
+use Web::App::Common;
+
 
 sub normalize_string($) {
   my $string = shift;
@@ -18,14 +23,162 @@ sub normalize_string($) {
   for ( $string ) {
     $_ = NFD( $_ );
     $_ = uc $_;
-    s!([,\.])!$1 !g;
-    s!\.\s+,!.,!g;
+    s/[^\w\d\s]/ /g;
+#    tr/[\.,-'"`]/ /;
+#    s!([,\.])!$1 !g;
+#    s!\.\s+,!.,!g;
     s!\s\s+! !g;
-    s!(^\s+|\s+$)!!g;
+    s!(^\s|\s$)!!g;
   }
 
   return $string;
 }
+
+sub build_citations_index($;$) {
+  my ( $citlist, $index ) = @_;
+  $index |= {};
+  
+  foreach (@$citlist) {
+    my $key = $_->{srcdocsid} . '-' . $_->{checksum};
+    $index ->{$key} = $_;
+  }
+  return $index;
+}
+
+
+use String::Approx qw( amatch );
+use String::Similarity;
+
+
+
+sub cit_document_similarity {
+  my ( $cit, $doc ) = @_;
+  my $result = 0;
+
+  assert( $cit and $doc );
+
+  my $ostring = $cit->{ostring};
+  my $nstring = $cit->{nstring};
+  debug "cit_document_similarity: citation: ", $nstring;
+
+  # Look at the author names first. Split the names of the
+  # authors of the document into a list of words, strip all
+  # non-letter non-whitespace characters and then strip
+  # single whitespace-separated letters from it.
+
+  my $authorslist = $doc->{authors};
+  my $authornames = join ' ', @$authorslist;
+  for( $authornames ) {
+    s/[^\w]/ /g;
+    s/\b\w\b/ /g;
+    s/^\s+/ /g;
+    s/\s+$/ /g;
+  }
+  my $authornamelist = [ split /\s+/, $authornames ];
+
+  # We then search for approximate matches for each of these
+  # words in the normalized citation string.  If there is a
+  # match for any of the author-name-words, it is an "author
+  # pass". Otherwise, the match is aborted with zero
+  # similarity measure returned.
+
+  my $pass;
+  foreach ( @$authornamelist ) {
+    debug "author name word $_";
+    if ( amatch( "$_ ", $nstring ) ) { $pass = 1; last; }
+  }
+  
+  if ( not $pass ) {
+    return 0;
+  }
+
+  # If the document is an author pass, it will be ranked
+  # according to the string similarity of the title only.
+  # Compare titles. 
+
+  # Take the normalized citation string and take the
+  # normalized title of the research item. 
+  my $ntitle = normalize_string( $doc->{title} );
+
+
+  # check title length
+  if ( length( $ntitle ) <= 10 ) {
+    debug "too short a title: ", $ntitle;
+    # XXX
+    return 0;
+  }
+  
+
+  # Find where in the citation string the first word of the
+  # title is present.
+
+  my @titlewords = split /\s+/, $ntitle;
+  
+  if ( scalar @titlewords < 2 ) {
+    debug "one word title? ", $ntitle; 
+    # XXX
+    return 0;
+  }
+  
+  my $first = $titlewords[0];
+  while ( length( $first ) < 2 ) {
+    shift @titlewords;
+    $first .= ' ' . $titlewords[0];
+  }
+  debug "first word of the document's title is: '$first'";
+  
+  my $startpos = index $nstring, $first;
+  debug "citation has title starting at: $startpos";
+
+  if ($startpos) {
+    # now compare
+    my $cittitle = substr $nstring, $startpos, length( $ntitle );
+    $result = similarity $ntitle, $cittitle;
+
+  } else {
+    debug "the title's begining was not found in the nstring; how do we compare?";
+    # XXX
+
+  }
+
+
+  return $result;
+} 
+
+
+sub p (@) { print @_, "\n"; }
+
+sub test_cit_document_similarity() {
+  
+  my @docs = (
+              { title=> "INEQUALITY DECOMPOSITION ANALYSIS AND THE GINI COEFFICIENT REVISITED",
+                authors => ['Peter J. Lambert', 'J. Richard ARONSON', 'ANAND' ],
+                },
+              );
+
+  my @ncits = (
+"LAMBERT P J AND J R ARONSON 1993 INEQUALITY DECOMPOSITION ANALYSIS AND THE GINI COECIENT REVISITED ECONOMIC JOURNAL 103",
+"ANAND S 1983 INEQUALITY AND POVERTY IN MALAYSIA MEASUREMENT AND DECOMPOSITION OXFORD UNIVERSITY PRESS NEW YORK",
+"BRUNO MICHAEL AND JEFFREY SACHS 1985 ECONOMICS OF WORLDWIDE STAGFLATION OXFORD BASIL BLACKWELL",
+              );
+
+  p "comparing";
+  foreach ( @docs ) {
+    my $d = $_;
+    p "\ndoc: ", $_->{title};
+
+    foreach ( @ncits ) {
+      p "cit: $_";
+      my $sim = cit_document_similarity( { nstring => $_ }, $d );
+      p "result: ", $sim;
+    }
+  }
+}
+
+
+
+
+
 
 
 sub get_document_authors($) {
