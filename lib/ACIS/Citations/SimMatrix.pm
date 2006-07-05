1a5392aaf0080f8d4d6daf749cd8b3bff2c39b5f
Ivan Kurmanov <duraley@gmail.com>
2006-07-05 07:28:59 -0400

    [project @ 2006-07-05 11:28:59 by ivan]
    a number of new advanced methods added and citation index
    
    darcs-hash:20060705112859-a15f8-954ad95ee9da3cea0b2bdce1b460d98f63a6aea2.gz

--- lib/ACIS/Citations/SimMatrix.pm
+++ lib/ACIS/Citations/SimMatrix.pm
@@ -32,13 +32,14 @@ use vars qw( @EXPORT_OK );
 
 
 use ACIS::Citations::Suggestions qw( load_suggestions );
-
+use Web::App::Common;
 
 sub load_similarity_matrix($) {
-  my $psid = shift;
+  my $psid = shift || die;
   
+  debug "load_similarity_matrix( '$psid' )";
   my $sug  = load_suggestions( $psid );
-  my $mat  = { new => {}, old => {} };
+  my $mat  = { new => {}, old => {}, psid => $psid, citations=>{} };
 
   bless $mat;
 
@@ -52,15 +53,22 @@ sub load_similarity_matrix($) {
 }
 
 sub _add_sug {
-  my $self = shift;
-  my $sug  = shift;
+  my $self = shift || die;
+  my $sug  = shift || die;
   
-  my $d      = $sug ->{dsid};
+  my $d      = $sug ->{dsid} || die;
   my $newold = ($sug->{new}) ? 'new' : 'old';
+
+  my $known = $self->{citations};
 
   for ( $self->{$newold}{$d} ) {
     if ( not $_ ) { $_ = []; }
     push @$_, $sug;
+
+    # maintain an index
+    my $cid = $sug->{srcdocsid} . '-' . $sug->{checksum};
+    my $cindex = $known->{$cid}{$d} ||= [];
+    push @$cindex, [ $newold, $sug ];
 
     # clear redundant bits
     delete $sug->{dsid};
@@ -106,6 +114,49 @@ sub most_interesting_doc {
 }
 
 
+sub filter_out_known {
+  my $self   = shift || die;
+  my $list   = shift || die;
+  my $reason = shift;
+
+  debug "filter_out_known()";
+
+  my $known = [];
+  my $citindex = $self->{citations};
+
+  foreach ( @$list ) {
+    my $citation = $_;
+
+    my $cid = $citation->{srcdocsid} . '-' . $citation->{checksum};
+    my $found;
+
+    if ( $citindex->{$cid} ) {
+      # known 
+      if ( $reason ) {
+        my $a = $citindex->{$cid};
+        foreach ( @$a ) {
+          my $_reason = $_->[1]->{reason};
+          if ( $reason eq $_reason ) { $found = 1; last; }
+        }
+
+      } else { 
+        $found = 1; 
+      }
+    }
+
+    if ( $found ) {
+      debug "citation known: $cid";
+      push @$known, $_;
+      undef $_;
+    }
+
+  }
+
+  clear_undefined $list;
+  return $known;
+}  
+
+
 
 
 sub testme {
@@ -120,12 +171,208 @@ sub testme {
 
 }
 
+package ACIS::Citations::SimMatrix; # ::Manager 
+
+use strict;
+use warnings;
+use Carp::Assert;
+use ACIS::Citations::Suggestions qw( load_suggestions replace_suggestion store_similarity );
+use Web::App::Common;
+
+sub upgrade {
+  my $self = shift || die;
+  my $acis = shift || die;
+  my $rec  = shift || die;
+
+  assert( $acis->{home} and $acis->{screenconf} );
+  assert( $rec->{name}  and $rec->{id} );
+
+  $self->{acis} = $acis;
+  $self->{rec}  = $rec;
+}
+
+sub find_cit {
+  my $self   = shift || die;
+  my $cit    = shift || die;
+
+  my $known = $self->{citations};
+  
+  # check the index
+  my $cid = $cit->{srcdocsid} . '-' . $cit->{checksum};
+
+  return $known->{$cid};
+}
+
+sub find_sugg {
+  my $self   = shift || die;
+  my $cit    = shift || die;
+  my $dsid   = shift || die;
+  my $reason = shift || die;
+
+  my $known = $self->{citations};
+  
+  # check the index
+  my $cid = $cit->{srcdocsid} . '-' . $cit->{checksum};
+
+  my $l = $known->{$cid}{$dsid};
+  foreach ( @$l ) {
+    # $_->[0] new / old
+    # $_->[1] suggestion itself
+    if ( $_->[1]->{reason} eq $reason ) {
+      return @$_;
+    }
+  }
+
+  return ();
+}
+
+
+sub add_sugg {
+  my $self   = shift || die;
+  my $cit    = shift || die;
+  my $dsid   = shift || die;
+  my $reason = shift || die;
+  my $sim    = shift;
+
+  my $psid = $self->{psid} || die;
+  my $l    = $self->{new} {$dsid} ||= [];
+
+  my $sug = { %$cit, reason => $reason, similar => $sim };
+  push @$l, $sug;
+
+  # maintain the index
+  my $known = $self->{citations};
+  my $cid   = $sug->{srcdocsid} . '-' . $sug->{checksum};
+  my $cindex = $known->{$cid}{$dsid} ||= [];
+  push @$cindex, [ 'new', $sug ];
+  
+  add_suggestion( $cit, $psid, $dsid, $reason, $sim );
+}
+
+sub set_similarity_unused {
+  my $self = shift || die;
+  my $cit  = shift || die;
+  my $dsid = shift || die;
+  my $sim  = shift;
+  my $new  = shift;
+
+  my $l = $self->{new} {$dsid} ||= [];
+  if ( not $new ) {
+    $l = $self->{old} {$dsid} ||= [];
+  }
+  
+  my $found;
+  foreach ( @$l ) {
+    if ( $_ ->{srcdocsid} eq $cit->{srcdocsid} 
+         and $_->{checksum} eq $cit->{checksum} ) {
+      $_ ->{similar} = $sim;
+      $found = 1;
+    }
+  }
+
+  if ( not $found ) {
+    die "set_similarity(): suggestion was not found";
+#    my $sug = { %$cit, reason => "similar", similar => $sim };
+#    push @$l, $sug;
+  }
+
+  replace_suggestion( $cit, $self->{psid}, $dsid, "similar", $sim, $new );
+
+#  warn "suggestion was not found" if not $found;
+}
+
+  
+
+sub compare_citation_to_documents {
+  my $self = shift || die;
+  my $cit  = shift || die;
+
+  debug "compare_citation_do_documents()";
+
+  my $docs = $self-> {docs};
+  if ( not $docs ) {
+    ### prepare doc objects, as per Similarity assessment function interface
+    $docs = {};
+    my $rp = $self->{rec}{contributions}{accepted} || [];
+    foreach ( @$rp ) {
+      my $doc = { %$_ };
+      $doc->{authors} = [ split / \& /, $doc->{authors} ];
+      if ( not $doc->{location} ) { }  # XXX 
+      $docs -> {$_->{sid}} = $doc;
+    }
+  }
+
+  my $psid = $self->{psid} || die;
+  my $acis = $self->{acis} || die;
+  my $func = $acis->config( 'citation-document-similarity-func' ) 
+    || 'ACIS::Citations::Utils::cit_document_similarity';
+
+  debug "will use similarity function: $func";
+
+  foreach ( my( $dsid, $doc ) = each %$docs ) {
+    no strict 'refs';
+    debug "comparing to $dsid (", $doc->{title}, ")";
+
+    my $similarity = &{$func}( $cit, $doc ) * 100.0;
+    debug "similarity: $similarity";
+
+#    my $sug = check_suggestions( $cit, $psid, $dsid, 'similar' );
+    my ($no, $sug) = $self->find_sugg(  $cit, $dsid, 'similar' );
+
+    if ( $sug ) {
+      debug "replacing";
+      my $newsug = ( $no eq 'new' ) ? 1 : 0;
+      $sug->{similar} = $similarity;
+      replace_suggestion( $cit, $psid, $dsid, "similar", $similarity, $newsug );
+#      $self->set_similarity( $cit, $dsid, $similarity, $newsug );
+
+    } else {
+      debug "adding";
+      $self->add_sugg( $cit, $dsid, "similar", $similarity );
+    } 
+  }
+
+  return 1;
+}
 
 
 
+sub add_new_citations {
+  my $self = shift || die;
+  my $list = shift || die;
+  my $dsid = shift;
+  my $reason = shift;
+
+  my $psid = $self->{psid} || die;
+  
+  if ( $dsid ) {
+    foreach ( @$list ) {
+      $self->add_sugg( $_, $dsid, "pre-identified", undef );
+    }
+
+  } else {
+    ### run comparisons
+    foreach ( @$list ) {
+      $self->compare_citation_to_documents( $_ );
+    }
+  }
+
+  $self -> _calculate_totals;
+
+  
+}
 
 
+sub run_maintenance {   # [60 min]
+  my $self = shift || die;
+}
 
+sub remove_citation {  # [30 min]
+  my $self = shift || die;
+  my $cit  = shift || die; 
+
+  # 
+}
 
 
 
