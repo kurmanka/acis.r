1697d89dac4c5eedd7a00953bf98acbab11da509
Ivan Kurmanov <duraley@gmail.com>
2006-07-18 08:33:18 -0400

    [project @ 2006-07-18 12:33:18 by ivan]
    now processing type is included in the queue
    
    darcs-hash:20060718123318-a15f8-6d1be22262bb0b84b1699dc6edc6f997f996b346.gz

--- lib/ACIS/APU.pm
+++ lib/ACIS/APU.pm
@@ -17,11 +17,6 @@ use sql_helper;
 use vars qw( $ACIS );
 *ACIS = *ACIS::Web::ACIS;
 
-my $QUEUE_TABLE_NAME = "arpm_queue";
-my $apu_too_recent_hours = 24;  # that's what is too recent for
-                                # running APU again for a
-                                # particular record
-
 my $interactive;
 my $logfile;
 
@@ -45,9 +40,8 @@ sub logit (@) {
 }
 
 
-# assumes $QUEUE_TABLE_NAME, $ACIS, @queries
-
-my @queries = ( 
+my $QUEUE_TABLE_NAME = "arpm_queue";
+my @QUERIES = ( 
     # select explicitly queue-ed records
     qq! SELECT what FROM $QUEUE_TABLE_NAME WHERE status='' 
         ORDER BY class DESC,filed ASC
@@ -55,7 +49,7 @@ my @queries = (
 
     # select records, which don't yet have a non-null value of
     # last-auto-citations-time for them
-    qq! SELECT r.shortid as what, s.data as data
+    qq! SELECT r.shortid as what, s.data as data, s.param as param
     FROM records as r LEFT JOIN sysprof as s 
        ON s.id = r.shortid  AND 
           s.param = 'last-auto-citations-time'    
@@ -63,10 +57,12 @@ my @queries = (
     LIMIT ? !,
 
     # select records, for which we ran APU most long ago
-    qq! SELECT id as what,data FROM sysprof  
+    qq! SELECT id as what,data,param FROM sysprof  
         WHERE param='last-autosearch-time' or param='last-auto-citations-time'
         ORDER BY data+1 ASC LIMIT ? !
 );
+
+# assumes $QUEUE_TABLE_NAME, $ACIS, @QUERIES
 
 sub prepare_to_process_queue {
   my $size = shift || die;
@@ -74,15 +70,19 @@ sub prepare_to_process_queue {
   logit "prepare the process queue: size=$size";
 
   my $sql = $ACIS -> sql_object || die;
-  my $get = ($size < 3) ? 6 : $size * 2;
+  my $get = ($size < 3) ? 9 : $size * 3;
   my @skipped; 
   my @to_process_logins;
   my @items;
-  my $by_lastsearch;
+  my %items_hash;
   my $r;
+  my @q = @QUERIES;
+
+  my $apu_too_recent_days  = $ACIS->config( 'minimum-apu-period-days' ) || 21;
+  my $apu_too_recent_hours = $apu_too_recent_days * 24;
 
   # first source of the queue items
-  my $query = shift( @queries );
+  my $query = shift( @q );
   $sql -> prepare( $query );
   $r = $sql -> execute( $get );
 
@@ -90,14 +90,26 @@ sub prepare_to_process_queue {
   while ( 1 ) {
     my $item =    $r -> {row} {what};
     my $lastrun = $r -> {row} {data};
+    my $param   = $r -> {row} {param};
+    my $type;
+
+    if ( $param ) {
+      if ( $param eq 'last-autosearch-time' ) {
+        $type = 'research';
+      } elsif ( $param eq 'last-auto-citations-time' ) {
+        $type = 'citations';
+      }
+    }
 
     if ( ! $item ) { next; }
+    if ( $items_hash{$item} ) { next; }
 
-    if ( $by_lastsearch ) {
+    if ( $lastrun ) {
       my $last = $lastrun || die;
       my $now  = time;
       if ( $now - $last <= $apu_too_recent_hours * 60 * 60 ) {
         # are we running too fast?
+        # XXX slow down APU throughput
 #       logit "skipping $item";
         push @skipped, $item;
         next;
@@ -108,14 +120,15 @@ sub prepare_to_process_queue {
     my $login = get_login_from_queue_item( $ACIS, $item );
     if ( $login ) {
       push @items, $item;
-      push @to_process, [ $login, $item, $lastrun ];
+      push @to_process, [ $login, $item, $lastrun, $type ];
       push @to_process_logins, $login;
+      $items_hash{$item} = 1;
 
       $get--;
       if ( not $get ) { last; }
       
     } else {
-      if ( not $by_lastsearch ) {
+      if ( not $lastrun ) {
         $sql -> prepare_cached( "delete from $QUEUE_TABLE_NAME where what=?" );
       } else {
         $sql -> prepare_cached( "delete from sysprof where id=?" );
@@ -129,12 +142,10 @@ sub prepare_to_process_queue {
     
     if ( not $r -> next ) {
     QUERY:
-      if ( scalar @queries ) {
-        $query = shift( @queries );
+      if ( scalar @q ) {
+        $query = shift( @q );
         $sql -> prepare( $query );
         $r = $sql -> execute( $get );
-        
-        $by_lastsearch = ($query =~ /\sFROM sysprof\s/i );
       } else { last; }
 
       if ( not $r or !$r->{row} ) { goto QUERY; }
