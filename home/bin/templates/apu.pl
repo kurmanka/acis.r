928b432fe05947b641e0ebffc009ee1ff0489f2b
Ivan Kurmanov <duraley@gmail.com>
2006-07-25 10:25:26 -0400

    [project @ 2006-07-25 14:25:26 by ivan]
    apu script
    
    darcs-hash:20060725142526-a15f8-a645e7541ac17432cad52bf94bf173143f56fac1.gz

---
+++ home/bin/templates/apu.pl
@@ -0,0 +1,33 @@
+
+use Carp::Assert;
+
+use sql_helper;
+use ACIS::Web;
+
+use Web::App::Common;
+
+
+use warnings;
+
+require ACIS::APU;
+
+
+#####  MAIN PART  
+
+my $acis = ACIS::Web -> new( home => $homedir );
+
+my $pretend;
+foreach ( @::ARGV ) {
+  if ( m/^--pre(t(e(nd?)?)?)?$/ ) {
+    $pretend = 1;
+    undef $_;
+  }
+}
+clear_undefined( \@::ARGV );
+
+my $howmuch = shift @ARGV;
+
+
+
+ACIS::APU::run_apu_by_queue($howmuch, $pretend);
+
