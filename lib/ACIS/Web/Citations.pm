41f1c2805fcb991f145ba2f8676e944b5f99fdca
Ivan Kurmanov <duraley@gmail.com>
2006-07-26 17:30:37 -0400

    [project @ 2006-07-26 21:30:37 by ivan]
    citations/potential some initial work
    
    darcs-hash:20060726213037-a15f8-5a284c3130789f622bd45c3dbdb7bc96cedd5a81.gz

--- lib/ACIS/Web/Citations.pm
+++ lib/ACIS/Web/Citations.pm
@@ -1,9 +1,65 @@
 package ACIS::Web::Citations;
 
 use strict;
+use warnings;
+use Carp::Assert;
+
+use Web::App::Common;
+
+use ACIS::Citations::Utils;
+use ACIS::Citations::Suggestions;
+use ACIS::Citations::SimMatrix;
+use ACIS::Citations::Search;
 
 
+my $acis;
+my $sql;
 
+sub prepare() {
+  $acis = $ACIS::Web::ACIS;
+  $sql  = $acis -> sql_object;
+}
+
+sub acis_citations_enabled() {
+  prepare() if not $acis;
+  return $acis->config( 'citations-profile' );
+}
+
+sub prepare_potential {
+  shift;
+  die "citations not enabled system-wide" if not acis_citations_enabled;
+
+  my $dsid = shift || $acis->{request}{subscreen};
+  if ( !$dsid ) {
+    die "no document sid to show citations for";
+    return;
+  }
+
+  my $session = $acis -> session;
+  my $vars    = $acis -> variables;
+  my $record  = $session -> current_record;
+  my $id      = $record ->{id};
+  my $sid     = $record ->{sid};
+
+  my $document;
+  foreach ( @{ $record->{contributions}{accepted} } ) {
+    if ( $_->{sid} eq $dsid ) { 
+      $document = $_;
+    }    
+  }
+  
+  die "can't find that document: $dsid";
+
+#  my $mat = $session ->{simmatrix} ||= load_similarity_matrix( $sid );
+  my $mat = $session ->{simmatrix} ||= load_similarity_matrix( 'ptestsid0' ); # XXXXXXXX
+  my $citations_new = $mat->{new}{$dsid};
+  my $citations_old = $mat->{old}{$dsid};
+
+  $vars -> {document} = $document;
+  $vars -> {potential_new} = $citations_new;
+  $vars -> {potential_old} = $citations_old;
+  
+}
 
 
 
