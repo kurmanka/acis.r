5ef725e3a924e6eb9be20fb31a2f72f4b9a559bd
Ivan Kurmanov <duraley@gmail.com>
2007-02-01 02:06:23 -0500

    [project @ 2007-02-01 07:06:23 by ivan]
    a cleanup, and memleak debugging disabled
    
    darcs-hash:20070201070623-a15f8-499fea3433829885867eab92d11cbf20d1ba5010.gz

--- lib/ACIS/Web.pm
+++ lib/ACIS/Web.pm
@@ -6,10 +6,8 @@ package ACIS::Web;   ### -*-perl-*-
 #
 #    ACIS core
 #
-#
-#  Copyright (C) 2003 Ivan Baktcheev, Ivan Kurmanov for ACIS project,
+#  Copyright (C) 2003 Ivan Baktcheev, (C) 2003-2007 Ivan Kurmanov for ACIS project,
 #  http://acis.openlib.org/
-#
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License, version 2, as
@@ -25,86 +23,65 @@ package ACIS::Web;   ### -*-perl-*-
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
 #  ---
-#  $Id: Web.pm,v 2.10 2007/01/24 18:07:12 ivan Exp $
+#  $Id: Web.pm,v 2.11 2007/02/01 07:06:23 ivan Exp $
 #  ---
-
 
 use strict;
 use warnings;
 
 use Carp qw( verbose cluck );
 use Carp::Assert;
-
 use Data::Dumper;
 use Storable;
-
 use Encode;
 
 require sql_helper;
-
 require Web::App;
 use base qw( Web::App );
 
-
 require Web::App::XSLT;
 use Web::App::Common;
-
-
 use ACIS::Data::DumpXML qw(dump_xml);
-
 require ACIS::Web::Config;
 require ACIS::Web::Session;
 require ACIS::Web::Session::SNewUser;
 require ACIS::Web::Session::SOldUser;
 require ACIS::Web::Session::SMagic;
 
-
 use vars qw ( $ACIS %SESSION_CLASS $SESSION_CLASS_MAIN );
-
 *ACIS = *Web::App::APP;
-
-
 $SESSION_CLASS_MAIN = "ACIS::Web::Session";
-
 %SESSION_CLASS = (
  "user"     => "ACIS::Web::Session::SOldUser",
  "new-user" => "ACIS::Web::Session::SNewUser",
  "magic"    => "ACIS::Web::Session::SMagic",
 );
 
-
 require ACIS::Web::UserData;
 require ACIS::Web::Services;
 
 
-sub basename {
-  return 'acis';
-};
+sub basename { 'acis' };
 
 
 require ACIS::LocalConfig;
 
 sub new {
-
   my $class   = shift;
   my $params  = {@_};
   my $home    = $params -> {home};
   
   $home = $ACIS::LocalConfig::home_dir
-    unless ( defined $home );
-  
+                unless ( defined $home );
   assert( $home );
 
   $params -> {home}       = $home;
   $params -> {serializer} = \&dump_xml_serializer;
-  $params -> {'presentation-builder-func'} 
-    = \&Web::App::XSLT::presentation_builder;
+  $params -> {'presentation-builder-func'} = \&Web::App::XSLT::presentation_builder;
 
-  
   debug "creating ACIS::Web object in $home";
   
   my $self = $class -> SUPER::new ( %$params );
-
   if ( $self -> config( "log-profiling-data" ) ) {
     $self -> {LOG_PROFILING} = 1;
     require Web::App::Profiling;
@@ -117,16 +94,13 @@ sub new {
 
 # we do not use Web::App::FormsProcessing, so we do not need this:
 #  $self -> {CGI_UNTAINT_INCLUDE_PATH} = 'ACIS::Web::CGI::Untaint';
-
   return $self;
 }
 
 
 sub dump_xml_serializer {
   my $data = shift;
-
   my $ret = ACIS::Data::DumpXML::dump_wo_refs( $data );
-
   for ( $ret ) {
     ### an optional UTF8 validity check  XX 
     if ( Encode::is_utf8( $_ ) ) {
@@ -142,25 +116,16 @@ sub dump_xml_serializer {
 
 sub init_presenter_data {
   my $self = shift;
-  
+
   $self -> SUPER::init_presenter_data;
 
-  my $data = $self -> {'presenter-data'};
-
-  ###  copy some of the configuration parameters into
-  ###  $presenter-data/system/config
+  ### copy some of the configuration parameters into
+  ### presenter-data/system/config
   my $config = $self -> config;
-  
-  assert( $config -> {'metadata-db-name'} );
-  
-  my @config_params_copy = qw( institutions-maintainer-email 
-                               research-auto-search-disabled 
-                             );
-  
-  my $data_conf = $data -> {system} {config};
-  
-  foreach ( @config_params_copy ) {
-    $data_conf ->{$_} = $config ->{$_};
+  my @copy = qw( institutions-maintainer-email research-auto-search-disabled );
+  my $pconf = $self -> {'presenter-data'} {system} {config};
+  foreach ( @copy ) {
+    $pconf ->{$_} = $config ->{$_};
   }
 }
 
@@ -173,33 +138,26 @@ sub parse_request_url {
   my ( $screen, $session ) = $self -> SUPER::parse_request_url( $url );
 
   my $req  = $self -> {request};
-
   if ( $screen and $screen =~ m!^(\w+\d+)/(.+)! ) {  ### (p\w+\d+) XXX
     $screen = $2;
     $req -> {screen}     = $screen;
     $req -> {'short-id'} = $1;
   }
 
-  my $session_cook = $self -> get_cookie( 'session' );
-
-  if ( $session_cook ) {
-    debug "session id cookie: $session_cook";
-
+  my $cooki = $self -> get_cookie( 'session' );
+  if ( $cooki ) {
+    debug "session id cookie: $cooki";
     if ( $session ) {
-      if ( $session_cook ne $session ) {
-        ###  Conflict.  Which one shall we obey?
-
+      if ( $cooki ne $session ) { ###  Conflict. Which one should we obey?
         $self -> set_cookie( -name  => 'session',
                              -value => $session );
       }
     } else {
-      $session = $session_cook;
+      $session = $cooki;
       $req -> {'session-id'} = $session;
       $req -> {'session-id-from-cookie'} = 1;
     }
-
   }
-
   return ( $screen, $session );
 }
 
@@ -212,25 +170,20 @@ sub userdata_dir {
 
 
 sub find_right_screen {
-  my $app = shift;
+  my $self = shift;
   my $scr = shift;
-
   debug "find_right_screen: enter";
-
-  my $res = $app -> SUPER::find_right_screen( $scr );
+  my $res = $self -> SUPER::find_right_screen( $scr );
 
   if ( not $res ) {
-
     require ACIS::Web::Site;
-    my $r = ACIS::Web::Site::serve_local_document( $app );
-    
+    my $r = ACIS::Web::Site::serve_local_document( $self );
     if ( $r ) {
       debug "find_right_screen: exit with putting responsibility on <local-document>";
-      $app -> request -> {responsibility_of} = "local-document";
+      $self -> request -> {responsibility_of} = "local-document";
       return $scr;
     }
   }
-
   no warnings;
   debug "find_right_screen: exit with <$res>";
   return $res;
@@ -239,15 +192,15 @@ sub find_right_screen {
 
 
 sub set_auth_cookies { 
-  my $app   = shift;
+  my $self  = shift;
   my $login = shift;
   my $pass  = shift;
 
   if ( defined $login ) {
-    $app -> set_authentication_cookie( 'login', $login );
+    $self -> set_authentication_cookie( 'login', $login );
   }
   if ( defined $pass ) {
-    $app -> set_authentication_cookie( 'pass' , $pass  );
+    $self -> set_authentication_cookie( 'pass' , $pass  );
   }
   debug "authentication cookies set:" . ( ($login) ? " login" : "" ) . 
     ( ($pass) ? " pass": "" );
@@ -255,10 +208,9 @@ sub set_auth_cookies {
 
 
 sub clear_auth_cookies { 
-  my $app   = shift;
-
-  $app -> set_authentication_cookie( 'login', '', 1 );
-  $app -> set_authentication_cookie( 'pass' , '', 1 );
+  my $self = shift;
+  $self -> set_authentication_cookie( 'login', '', 1 );
+  $self -> set_authentication_cookie( 'pass' , '', 1 );
 }
 
 sub get_auto_logon_mode {
@@ -287,17 +239,11 @@ sub get_auto_logon_mode {
 }
 
 
-
 sub set_authentication_cookie { 
-  my $app   = shift;
-  my $name  = shift;
-  my $value = shift;
-  my $clear = shift;
-
-  $app -> set_cookie( -name  => $name,
-                      -value => $value,
-                      -expires => ($clear) ? '+0m' : '+6M',
-                    );
+  my ($self, $name, $value, $clear) = @_;
+  $self -> set_cookie( -name  => $name,
+                       -value => $value,
+                     -expires => ($clear) ? '+0m' : '+6M', );
 }
 
 
@@ -305,24 +251,19 @@ sub prepare_presenter_data {
   my $self = shift;
   $self -> SUPER::prepare_presenter_data();
   
-  if ( defined $self ->{request} {'short-id'} ) {
-    $self -> {'presenter-data'} {request} {'short-id'} = 
-      $self ->{request} {'short-id'};
+  if ( defined $self->{request}{'short-id'} ) {
+    $self->{'presenter-data'}{request}{'short-id'} = $self->{request}{'short-id'};
   }
-
 }
 
 sub redirect_to_screen_for_record {
   my $self   = shift;
   my $screen = shift;
   
-  my $rec_sid = $self -> session -> current_record -> {sid};
-  if ( $rec_sid =~ m/^p/ ) { 
-    ###  real short id
-    $self -> redirect_to_screen( "$rec_sid/$screen" );  
-
-  } else {
-    ###  short id placeholder
+  my $sid = $self -> session -> current_record -> {sid};
+  if ( substr( $sid, 0, 1 ) eq 'p' ) { ### a real shortid
+    $self -> redirect_to_screen( "$sid/$screen" );  
+  } else {                             ### a shortid placeholder
     $self -> redirect_to_screen( $screen );  
   }    
 }
@@ -331,12 +272,10 @@ sub redirect_to_screen_for_record {
 sub get_url_of_a_screen {
   my $self   = shift;
   my $screen = shift;
-
   my $rec_sid = $self -> session -> current_record -> {sid};
   if ( $rec_sid =~ m/^p/ ) { 
     $screen = "$rec_sid/$screen";
   }
-  
   return $self -> SUPER::get_url_of_a_screen( $screen );
 }
 
@@ -344,26 +283,18 @@ sub get_url_of_a_screen {
 sub userdata_file_for_login {
   my $self  = shift;
   my $login = shift || die;
-
   $login = lc $login;
-
   my $word_login = $login;
   $word_login =~ s/[^a-z0-9]//g;
-
   my ( $fl, $sl ) = unpack( 'aa', $word_login );
-
   my $udata_dir  = $self -> userdata_dir;
-
   my $safe = $login;
-  $safe =~ s![\s|;><\\/]!!g;  ### security
-
+  $safe =~ s![\s|;><\\/]!!g;  ### security - remove unsafe characters
   my $udata_file = "$udata_dir/$fl/$sl/$safe.xml";
 
-  if ( not -f $udata_file ) { ### XX this sometimes creates spurious
-                              ### directories
+  if ( not -f $udata_file ) { ### this sometimes creates spurious directories
     force_dir( $udata_dir, "$fl/$sl");
   }
-
   return $udata_file;
 }
 
@@ -373,7 +304,6 @@ sub make_paths_for_login {
   my $self  = shift;
   my $login = shift || die;
   my $paths = shift || {};
-
   my $udata_file = $self -> userdata_file_for_login( $login );
   my $udata_lock = "${udata_file}.lock";
 
@@ -385,9 +315,8 @@ sub make_paths_for_login {
   my $safe    = lc $login;
   $safe =~ s![|;><\\/]!!g;  ### drop unsafe characters
   my $deleted = "${homedir}/deleted-userdata/${safe}.xml"; 
-  
   $paths -> { 'user-data-deleted' } = $deleted; 
-  
+
   return $paths;
 }  
 
@@ -407,90 +336,70 @@ sub update_paths_for_login {
 sub session {
   my $self    = shift;
   my $session = shift;
+  if ( not defined $session ) { return $self->{session}; }
 
-  if ( defined $session ) {
+  $self -> SUPER::session( $session );
 
-    $self -> SUPER::session( $session );
-    
-    my $prequest =  $self -> {'presenter-data'} {request};
-
-    if ( not $session -> owner ->{login} ) {
-      return $session;
-    }
-
-    $self -> set_username( $session -> owner -> {login} );
-
-    {
-      my $id       = $session ->id;
-      my $type     = $session ->type;
-      my $userdata = $session ->object;
-      my $realuser = '';
-      if ( $userdata ) { $realuser = $userdata->{owner}->{login}; }
-      if ( $realuser ) { $realuser = " for user $realuser";  }
-#      $self -> userlog ( "using session $id ($type)$realuser" );
-    }
-
-    my $record = $session -> current_record;
-  
-    if ( $record ) {
-
-      my $cur_rec = {
-                       'id' => $record -> {id},
-                     'name' => $record -> {name} {full},
-                     'type' => $record -> {type},
-                  'shortid' => $record -> {sid},
-                    };
-
-      if ( $record->{'about-owner'} ) {
-        $cur_rec ->{'about-owner'} = $record ->{'about-owner'};
-      }
-
-      $prequest -> {session} {'current-record'} = $cur_rec;
-    }
-
+  if ( not $session -> owner ->{login} ) {
     return $session;
   }
+  $self -> set_username( $session -> owner -> {login} );
+    
+  my $id       = $session ->id;
+  my $type     = $session ->type;
+  my $userdata = $session ->object;
+  my $realuser = '';
+  if ( $userdata ) { $realuser = $userdata->{owner}->{login}; }
+  if ( $realuser ) { $realuser = " for user $realuser";  }
+  #$self -> userlog ( "using session $id ($type)$realuser" );
 
-  return $self -> {session};
+  my $record = $session -> current_record;
+  if ( $record ) {
+    my $cur_rec = {
+                   id      => $record -> {id},
+                   name    => $record -> {name} {full},
+                   type    => $record -> {type},
+                   shortid => $record -> {sid},             };
+    if ( $record->{'about-owner'} ) {
+      $cur_rec ->{'about-owner'} = $record ->{'about-owner'};
+    }
+    my $preq = $self -> {'presenter-data'} {request};
+    $preq -> {session} {'current-record'} = $cur_rec;
+  }
+  return $session;
 }
 
 
-
-
-
 sub update_paths {
-
   my $self  = shift;
   my $paths = $self ->{paths};
-  
   if ( defined $self -> {session} ) {
-
     my $session = $self -> session;
     my $home    = $self -> {home};
-
     my $userdata = $session ->object;
     
     if ( $userdata ) {
       my $login    = $userdata ->{owner} ->{login};
       my $oldlogin = $userdata ->{owner} ->{'old-login'};
-
       if ( $login ) {
         $paths = $self ->update_paths_for_login( $login );
       }
-
       if ( $oldlogin ) {
         my $old_userdata_file = $self ->userdata_file_for_login( $oldlogin );
-        #      my ( $old_uprefix ) = ( $old_userdata_file =~ /(.+)\.xml$/ );
-      
         $paths -> {'user-data-lock'} = "$old_userdata_file.lock";
         $paths -> {'user-data-old' } = $old_userdata_file;
-        
       } # if $oldlogin
     }
-
   } 
-
   return $paths;
+}
+
+
+sub clear_after_request {
+  my $self = shift;
+  if ( $ACIS::Web::Citations::{cleanup} ) { ACIS::Web::Citations::cleanup(); }
+  if ( $ACIS::Web::Contributions::{cleanup} ) { ACIS::Web::Contributions::cleanup(); }
+  $self-> SUPER::clear_after_request();
 }
 
 
@@ -511,7 +420,6 @@ my $insert_event_query =
 sub event {
   my $self = shift;
   my %p    = @_;
-
   my $sql = $self -> sql_object;
 
   if ( not $p{-data} ) {
@@ -532,7 +440,6 @@ sub event {
   foreach ( @events_fields ) {
     push @val, $p{"-$_"};
   }
-
   $sql -> prepare_cached( $insert_event_query );
   $sql -> execute( @val );
 }
@@ -542,19 +449,12 @@ sub event {
 sub get_recent_events {
    my $self = shift; 
    my $period = shift || 24; ### hours
-
    my $sql = $self -> sql_object();
-
    my $stat = "select * from events where date > DATE_SUB( NOW(), INTERVAL ? HOUR) ".
      "ORDER BY date ASC, startend DESC ";
-   
    $sql -> prepare_cached( $stat );
    my $r = $sql -> execute( $period );
-
-   if ( $r ) {
-     ### 
-     return $r;
-   }
+   if ( $r ) { return $r; }
    return undef;
 }
 
@@ -563,16 +463,12 @@ sub get_recent_events {
 
 sub dump_presenter_xml {
   my $self = shift;
-  
   $self -> prepare_presenter_data;
   my $xml = $self -> serialize_presenter_data;
-
   my $response = $self-> {response};
-
   undef $response -> {HTML};
   $response -> {body} = $xml;
   $response -> {charset} = 'utf-8';
-
   print STDOUT "Content-type: application/xml; charset=utf-8\n";
   $response -> {'content-type-printed'} = 1;  
 }
@@ -580,22 +476,16 @@ sub dump_presenter_xml {
 
 sub dump_variables_xml {
   my $self = shift;
-  
   $self -> {'presenter-data'} = $self -> variables;  
   my $xml = $self -> serialize_presenter_data;
-
   my $response = $self-> {response};
-
   undef $response -> {HTML};
   $response -> {body} = $xml;
   $response -> {charset} = 'utf-8';
-
   debug "xml response: $xml";
-
   print STDOUT "Content-type: application/xml; charset=utf-8\n";
   $response -> {'content-type-printed'} = 1;  
 }
-
 
 
 sub sql { $_[0]->sql_object }
@@ -609,29 +499,19 @@ sub send_mail {
   my $stylesheet = shift;
   
   debug "sending email with template '$stylesheet'";
-
   my $config = $self -> config;
-
   my $message = $self -> run_presenter( $stylesheet );
 
-  
   my ($header, $body) = ($message =~ /^(.*?)\s*\n\n+(.*)$/s);
-
-  my @headers = split (/\s*\n/, $header);
-
+  my @headers = split /\s*\n/, $header;
 #  debug "Headers: $header\n";
-
   $header = '';
-
   my %head = ();
-
 #  $self -> log( "Sending an email.  The headers:" );
-
   foreach ( @headers ) {
     my ( $name, $value ) = split (/:\s*/, $_);
     $head{ lc $name } = $value;
     my $val = encode( 'MIME-Q', $value );
-
  #   $self -> log( "$name: $val" );
 
     ### XXX a nasty hack to fix Encode's "feature":
@@ -640,20 +520,16 @@ sub send_mail {
   }
   
 #  $self -> log( "Sending an email.  The headers:\n$header<<<" );
-
   $header .= "MIME-Version: 1.0\n";
   $header .= "Content-Type: text/plain; charset=utf-8\n";
   $header .= "Content-Transfer-Encoding: 8bit\n";
 
-
   my $sendmail = $config -> {sendmail};
-  
   unless ( defined $sendmail and $sendmail ) {
     debug "can't send email message, because no sendmail path defined";
     return;
   }
 
-  
   if ( open MESSAGE, "|-:utf8", $sendmail ) {
     print MESSAGE $header, "\n", $body;
     close MESSAGE;
@@ -663,23 +539,19 @@ sub send_mail {
     return undef;
   }
 
-
   my $to = $head{to};
   my $cc = $head{cc};
-
   $self -> sevent ( -class => 'email',
                    -action => 'sent',
                  -template => $stylesheet,
                        -to => $to,
                     ($cc) ? ( -cc => $cc ) : ()
                   );
-
 }
 
 
 #use Devel::LeakTrace;
-require Devel::Symdump;
-sub _debug_leaks_after_processing {
+sub _debug_leaks_after_processing__DISABLED {
   my $self = shift;
   
   use ACIS::Debug::MLeakDetect;
@@ -688,16 +560,14 @@ sub _debug_leaks_after_processing {
   use Data::Dump::Streamer;
   $self->log( Dump($self)->Out );
 #  print "<p>", Dump( $r )->Out(), "</p>";
-
   return;
 
-
-  my $dsd = Devel::Symdump->rnew(  );
+  require Devel::Symdump;
+  my $dsd = Devel::Symdump->rnew;
   my @packages = $dsd->packages;
   my @scalars  = $dsd->scalars;
 
-  print "Symbol dump:\n";
-
+#  print "Symbol dump:\n";
 #  print "<p>Packages: ";
 #  foreach (@packages) {
 #    print "$_ ";
@@ -705,7 +575,6 @@ sub _debug_leaks_after_processing {
 #  print "</p>";
 
   print "<p>Scalars: ";
-
   foreach (@scalars) {
     my $d;
     my $r;
@@ -718,15 +587,9 @@ sub _debug_leaks_after_processing {
   print "</p>";
 
   use Data::Structure::Util qw( has_circular_ref );
-
   my $r = has_circular_ref( $self );
-  if ( $r ) {
-  }
-
-
+  if ( $r ) {}
 }
-
-
  
 1;
 
@@ -742,6 +605,7 @@ ACIS::Web
  my $acis = new ACIS::Web ( 'home' => 't/acis-home' );
 
  $acis -> handle_request;
+ $acis -> clear_after_request;
 
 =head1 LICENSE
 
