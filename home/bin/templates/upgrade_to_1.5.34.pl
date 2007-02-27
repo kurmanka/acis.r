0ca8447eb4ec5076220e2759995a0397f2ae9f8c
Ivan Kurmanov <duraley@gmail.com>
2007-02-27 16:26:20 -0500

    [project @ 2007-02-27 21:26:20 by ivan]
    name columns size expanded to 255 chars
    
    darcs-hash:20070227212620-a15f8-36c6ce559d3b53499e3b2af34222476696bd1e41.gz

--- home/bin/templates/upgrade_to_1.5.34.pl
+++ home/bin/templates/upgrade_to_1.5.34.pl
@@ -9,8 +9,11 @@ my $sql = $ACIS -> sql_object;
 my $RDB = $ACIS -> config( 'metadata-db-name' );
 
 my @q = (
+  qq!alter table $RDB.res_creators_separate modify name varchar(255) not null!,
   qq!alter table $RDB.res_creators_separate add fulltext index creatorsft(name)!,
+  qq!alter table names modify name varchar(255) not null!,
   qq!alter table names add index namesindex(name)!,
+  qq!alter table users modify name varchar(255), modify login varchar(255) not null!,
 );
 
 print "please wait while we upgrade the database...\n";
