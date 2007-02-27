e14e84e780b3f64febe212faf5b1e83ec0c446b3
Ivan Kurmanov <duraley@gmail.com>
2007-02-27 06:35:36 -0500

    [project @ 2007-02-27 11:35:36 by ivan]
    name expressions fuzzy search limits
    
    darcs-hash:20070227113536-a15f8-a3f8a5e2a692da116adbedf0637fbd3b625ec38b.gz

--- home/bin/templates/fuzzy_search_table.pl
+++ home/bin/templates/fuzzy_search_table.pl
@@ -4,48 +4,46 @@ use warnings;
 use ACIS::Web;
 
 #####  MAIN PART
-my $ACIS = ACIS::Web -> new( home => $homedir );
-my $sql = $ACIS -> sql_object;
-my $RDB = $ACIS -> config( 'metadata-db-name' );
+my $acis = ACIS::Web -> new( home => $homedir );
+my $sql = $acis -> sql_object;
+my $RDB = $acis -> config( 'metadata-db-name' );
 
-my $doc_occur_limit = $ACIS->config( 'fuzzy-name-search-max-name-occurr-in-doc-names' );
-my $var_occur_limit = $ACIS->config( 'fuzzy-name-search-max-name-occurr-in-name-variations' );
+my $doc_occur_limit = $acis->config( 'fuzzy-name-search-max-name-occurr-in-doc-names' );
+if ( not defined $doc_occur_limit ) { $doc_occur_limit = 1; }
+my $var_occur_limit = $acis->config( 'fuzzy-name-search-max-name-occurr-in-name-variations' );
+if ( not defined $var_occur_limit ) { $var_occur_limit = 0; }
 
 if ( not $doc_occur_limit and not $var_occur_limit ) {
-  $self->sysvarset( "research.search.fuzzy.rare.names.table", undef );
+  $acis->sysvarset( "research.search.fuzzy.rare.names.table", undef );
   
 } else {
-
   my $having1st = '';
   if ( $doc_occur_limit ) {  $having1st = "having count(*)<= $doc_occur_limit";  }
-  
   my $having2nd = '';
-  if ( $var_occur_limit ) {  $having2nd = "having count(v.name)<= $var_occur_limit"; }
+  if ( defined $var_occur_limit 
+       and $var_occur_limit > -1 ) {  $having2nd = "having count(v.name)<= $var_occur_limit"; }
 
   # data flow:
   # RDB.res_creators_separate -> tmpnames -> tmpnames2 -> rare_names 
   my @q = (
-           qq!create temporary table tmpnames ( name varchar(100) not null, index namesi(name) )
+           qq!create temporary table tmpnames ( name varchar(255) not null, index namesi(name) )
               select name from $RDB.res_creators_separate group by name $having1st!,
-      # if $var_occur_limit:
-           qq!create temporary table tmpnames2 ( name varchar(100) not null, index namesi(name) )
-              select name from tmpnames left join names v using(name) group by name $having2nd!,
+           qq!create temporary table tmpnames2 ( name varchar(255) not null, index namesi(name) )
+              select t.name from tmpnames t left join names v using(name) group by name $having2nd!,
            qq!drop table tmpnames!,
-           qq!create table rare_names (name varchar(100) not null, sid char(12) not null, index namesi(name))!,
+           qq!create table rare_names (name varchar(255) not null, sid char(12) not null, primary key (name,sid))!,
            qq!replace into rare_names select t.name,r.sid from $RDB.res_creators_separate r join tmpnames2 t using(name)!,
            qq!delete from rare_names using rare_names left join tmpnames2 t using(name) where t.name is null!,
            qq!drop table tmpnames2!,
           );
 
-  print "please wait while we build the tables...\n";
-  
+  print "please wait while we build the rare names table...\n";
   foreach ( @q ) {
     $sql -> prepare( $_ );
-    print " $_\n";
+    print "  $_\n";
     $sql -> execute;
   }
-
   $acis->sysvarset( "research.search.fuzzy.rare.names.table", "rare_names" );
   
-  print "upgrade done.\n";
+  print "done.\n";
 }
--- home/bin/templates/upgrade_to_1.5.34.pl
+++ home/bin/templates/upgrade_to_1.5.34.pl
@@ -9,17 +9,15 @@ my $sql = $ACIS -> sql_object;
 my $RDB = $ACIS -> config( 'metadata-db-name' );
 
 my @q = (
-
-  q!alter table $RDB.res_creators_separate add fulltext index creatorsft(name)!,
-  q!alter table names add index namesindex(name)!,
-
+  qq!alter table $RDB.res_creators_separate add fulltext index creatorsft(name)!,
+  qq!alter table names add index namesindex(name)!,
 );
 
 print "please wait while we upgrade the database...\n";
 
 foreach ( @q ) {
   $sql -> prepare( $_ );
-  print " $_\n";
+  print "  $_\n";
   $sql -> execute;
 }
 
