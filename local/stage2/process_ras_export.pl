fcf242aea2a93dda570a396372c4077e843c118a
Ivan Kurmanov <duraley@gmail.com>
2007-07-04 08:38:16 -0400

    [project @ 2007-07-04 12:38:16 by ivan]
    statistics added
    
    darcs-hash:20070704123816-a15f8-1cd34745c9aa88ca704785c05577af687bf319c9.gz

--- local/stage2/process_ras_export.pl
+++ local/stage2/process_ras_export.pl
@@ -23,12 +23,19 @@ my $findref_st = $dbh->prepare( "select id from REFERENCIA where docid=? and md5
 my $addst = $dbh->prepare( "replace into CITA (cita,escitado,ref_cita,fecha) values (?,?,?,NOW())" ) or die;
 my $delst = $dbh->prepare( "delete from CITA where cita=? and escitado=?" ) or die;
 
+my $count_all = 0;
+my $count_added = 0;
+my $count_ref_not_found = 0;
+my $count_deleted = 0;
+
 while( <INPUT> ) {
   chomp;
   my ($srcdocid,$md5,$trgdocid,$event) = split( /\t/, $_ );
   if ( not $event 
        or not $srcdocid 
        or not $trgdocid ) { next; }
+  $count_all++;
+
   my $r = $findref_st->execute( $srcdocid, $md5 );
   my $array = $findref_st->fetchrow_arrayref
     if $r;
@@ -37,15 +44,21 @@ while( <INPUT> ) {
   
   if (not $refid) {
     print "no reference: $srcdocid $md5\n";
+    $count_ref_not_found++;
   }
   $refid ||= '';
   if ( $event eq 'added' 
        or $event eq 'autoadded' ) {
 #    print "add $refid -> $trgdocid\n";
     $addst->execute( $srcdocid, $trgdocid, $refid );
+    $count_added++;
+
   } elsif ( $event eq 'unidentified' ) {
 #    print "drop $refid -> $trgdocid\n";
     $delst->execute( $srcdocid, $trgdocid );
+    $count_deleted++;
   }
 }
+
+print "all: $count_all\t added: $count_added\t removed: $count_deleted\t ref not found: $count_ref_not_found\n";
 
