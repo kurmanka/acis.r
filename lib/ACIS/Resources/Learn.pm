28a787815fe48e1465ff95499193be25d2a441a0
ACIS <acis@openlib.org>
2009-07-27 08:15:53 -0400

    a new Learn.pm and Learn/Refused.pm, using libsvm binaries
    darcs-hash:20090727121553-854f8-c07fccfe03377dd5c548b2e5838d46a883fec17e.gz

--- lib/ACIS/Resources/Learn.pm
+++ lib/ACIS/Resources/Learn.pm
@@ -3,88 +3,238 @@ package ACIS::Resources::Learn;
 use Carp;
 use Carp::Assert;
 use Exporter;
+use File::Temp ();
 use base qw(Exporter);
 use vars qw(@EXPORT);
-use Storable qw(nfreeze thaw);
+use Storable qw(freeze thaw);
 use Web::App::Common;
 use ACIS::Web::Background qw(logit);
+use ACIS::Resources::Suggestions;
 use Data::Dumper;
+use sql_helper;
 use strict;
 
+# part of cardiff
+
+#
+# global variables
+#
+my $train_bin="/usr/bin/svm-train -q -b 1";
+my $predict_bin="/usr/bin/svm-predict -b 1";
 
 @EXPORT = qw( 
               sort_suggestions_through_learning();
+              learn_via_svm();
 );
 
 
 #
-# learn from suggestions
-# here it manipulates $app as to 
-# change the order of the suggestion
-sub sort_suggestions_through_learning {
-  my $app    = shift;
-
-  # these may be passed from the calling function
-  # but I am listing them separately here
-  my $session = $app -> session;
-  my $vars    = $app -> variables;
-  my $record  = $session -> current_record;
-  my $id      = $record -> {id};
-  my $psid    = $record -> {sid};
-  my $contributions=$app->{'variables'}->{'contributions'};
-  my $sql = $ACIS::Web::ACIS->sql_object;
-
-  # suggestions are grouped by reasons
-  my @groups=@{$contributions->{'suggest'}};
-  my $suggestion_count=0;
-  # suggestions united with no respect for reason 
-  my $suggestions;
-  foreach my $group (@groups) {
-    my $reason=$group->{'reason'};
-    foreach my $suggestion (@{$group->{'list'}}) {
-      # add the reason to earch suggestion so that we don't loose it      
-      $suggestion->{'reason'}=$reason;
-      # extremely primitive learning: relevance is the suggestion count
-      # this way learning will reverse the appearance of suggestions
-      $suggestion->{'relevance'}=$suggestion_count;
-      # add to an aggregaet $suggestions variable
-      $suggestions->[$suggestion_count]=$suggestion;
-      $suggestion_count++;
+# general learning function, 
+# takes accepetd, refused, suggested
+#
+sub learn_via_svm {
+  my $accepted=shift;
+  my $refused=shift;
+  # may be left out, then we learn about the 
+  # refused documents
+  my $suggested=shift;
+  my @accepted_docs=@{$accepted};
+  my $count_accepted=$#accepted_docs;
+  # there should be at least one accepted documet
+  if($count_accepted<0) {
+    return "no accepted documents";
+  }
+  # save memory
+  @accepted_docs=();
+  ##print "count_accepted is $count_accepted\n";
+  my @refused_docs=@{$refused};
+  my $count_refused=$#refused_docs;
+  ##print "count_refused is $count_refused\n";
+  my @suggested_docs;
+  my $count_suggested;
+  if(defined($suggested)) {
+    @suggested_docs=@{$suggested};
+    $count_suggested=$#suggested_docs;
+    @refused_docs=();
+  } 
+  else {
+    @suggested_docs=@{$refused};
+    $count_suggested=$#refused_docs;
+  }
+  # make sure it's worth proceeding
+  if($count_suggested < 1) {
+    return "not enough suggested documents";
+  }
+  # build the dataset
+  my $data;
+  foreach my $doc (@{$accepted}) {
+    $data=&add_document_to_data($data,$doc,'+1');
+  }
+  foreach my $doc (@{$refused}) {
+    $data=&add_document_to_data($data,$doc,'-1');
+  }
+  if($suggested) {
+    foreach my $doc (@{$suggested}) {
+      $data=&add_document_to_data($data,$doc,'0');
     }
   }
-
-  # now for some better learning, over to you Ilya...
-
-
-  # to save the suggestions, we have to group the $suggestions
-  # variable back to its reasons
-  # first let us count how many suggestions we have for earch
-  my $reason_size;
-  foreach my $suggestion (@{$suggestions}) {
-    $reason_size->{$suggestion->{reason}}++;
+  # now the document data lines are in $data->{'ds'}
+  my @ds=@{$data->{'ds'}};
+  # start with the trainingfile
+  my $train_fh = File::Temp->new();
+  my $train_file=$train_fh->filename;
+  # write the training set, leaving out docs
+  # that start with "0"
+  foreach my $doc_line (@ds) {
+    if(not $doc_line=~m|^0|) {
+      print $train_fh "$doc_line\n";
+    }
   }
-
-  #
-  # now save the suggestions, again grouped by reason
-  # 
-  foreach my $reason (sort {$reason_size->{$a} <=> $reason_size->{$b}}
-                      keys %{$reason_size}) {
-    # the group of suggentions having that reason
-    my $suggestions_for_reason;
-    my $suggestions_for_reason_count=0;
-    # compose the $suggestions_for_reason
-    foreach my $suggestion (@{$suggestions}) {
-      if($suggestion->{'reason'} eq $reason) {
-        $suggestions_for_reason->[$suggestions_for_reason_count]=$suggestion;
-        $suggestions_for_reason_count++;
+  $train_fh->close;
+  # create model file
+  my $model_fh=File::Temp->new();
+  my $model_file=$model_fh->filename();
+  # create system command to fire up training
+  my $s="$train_bin ".$train_fh->filename;
+  $s.=" $model_file";
+  # for debugging, save train file
+  ##$s.="; cp $train_file /tmp/train";
+  ##print "doing $s\n";
+  system($s);
+  # model is now trained, build the testing set
+  # organize test file
+  my $test_fh = File::Temp->new();
+  my $test_file=$test_fh->filename; 
+  # find the start of the suggested document
+  my $learn_doc_start;
+  if($suggested) {
+    $learn_doc_start=$count_accepted+$count_refused+1;
+  }
+  # if there are no suggested, start with the refused docs
+  else {
+    $learn_doc_start=$count_accepted+1
+  }  
+  foreach my $count_to_learn (0..$count_suggested) {
+    my $learn_doc=$data->{'ds'}->[$learn_doc_start+$count_to_learn];
+    # in case it was a refused document
+    $learn_doc=~s|^-1|0|;
+    ##print "wrote testing set line $count\n";
+    print $test_fh "$learn_doc\n";
+  }
+  $test_fh->close();
+  # organize out file
+  my $out_fh = File::Temp->new();    
+  my $out_file=$out_fh->filename();
+  # generate and read output file
+  $s="$predict_bin $test_file $model_file $out_file";
+  # for debuing
+  ##$s.="; cp $out_file /tmp/out";
+  ##$s.="; cp $test_file /tmp/test"; 
+  # for the output, REQUIRED
+  $s.="; cat $out_file";
+  ##print "doing: $s\n";
+  my $count_to_learn=0;
+  # read output and find the score for earch document
+  my $result;
+  foreach my $line (`$s`) {
+    ##print "$line";
+    if(not $line=~m|^[+-]*1|) {
+      next;
+    }
+    my @data=split(' ',$line);
+    my $score=$data[1];
+    ##print "score is $score\n";
+    push(@{$result->{$score}},$count_to_learn);
+    $count_to_learn++;
+  }  
+  my $docs_sorted;
+  my $docs_count;
+  # if there are no suggested documents, from here onwards consider
+  # the refused documents to be the suggested ones
+  if(not defined($suggested)) {
+    $suggested=$refused;
+  }
+  foreach my $number (sort {$b <=> $a} keys %{$result}) {
+    foreach my $doc_number (@{$result->{$number}}) {
+      push(@{$docs_sorted},$suggested->[$doc_number]);
+      if(not defined($suggested->[$doc_number])) {
+        print "fatal: document $doc_number is not defined!\n";
+        exit;
       }
     }
-    # save it
-    ACIS::Resources::Suggestions::save_suggestions( $sql, $psid, 
-                                                    $reason, undef, 
-                                                    $suggestions_for_reason );
   }
+  for(my $count=0; $count <= $count_suggested; $count++) {
+    if(not defined($docs_sorted->[$count])) {
+      print "fatal: docment $count is not defined!\n";
+      exit;
+    }
+    $suggested->[$count]=$docs_sorted->[$count];
+  }
+  ##my $out=Dumper $suggested;
+  ##print "$out" ;
+  return $suggested;
 }
 
+#
+# creates a line in the document
+#
+sub add_document_to_data {
+  my $data=shift;
+  my $doc=shift;
+  my $label=shift;
+  # form terms to look at
+  my @terms=split('\W',$doc->{'authors'});
+  push(@terms,split('\W',$doc->{'title'}));
+  # empty $data for this document;
+  #print "empty\n";
+  $data->{'doc'}={};
+  delete $data->{'doc'}->{'terms'};
+  delete $data->{'doc'}->{'position'};
+  foreach my $term (@terms) {
+    if(not length($term)>1) {
+      next;
+    }
+    $term=lc($term);
+    #print "term is '$term'\n";
+    # new term
+    my $term_number;
+    # new term 
+    if(not $data->{'terms'}->{'known'}->{$term}) {
+      $term_number=$data->{'terms'}->{'total_number'}++;
+      #print "term number is $term_number\n";
+      $data->{'terms'}->{'number'}->{$term}=$term_number;
+      $data->{'terms'}->{'known'}->{$term}=1;
+    }
+    else {
+      $term_number=$data->{'terms'}->{'number'}->{$term};
+    }
+    # count occurances of term
+    $data->{'doc'}->{'terms'}->[$term_number]++;
+    # keep track which term is actually used
+    $data->{'doc'}->{'position'}->{$term_number}=$term;
+    # length, as measure by terms, of the document
+    $data->{'doc'}->{'total'}++;
+  }
+  # now calculate weights such as the euclidean sum is one
+  my $line="$label ";
+  foreach my $position (sort {$a <=> $b} keys %{$data->{'doc'}->{'position'}}) {
+    my $raw_frequency=$data->{'doc'}->{'terms'}->[$position];
+    my $total=$data->{'doc'}->{'total'};
+    my $adjusted_frequency=sqrt($raw_frequency/$total);
+    $data->{'doc'}->{'terms'}->[$position]=$adjusted_frequency;
+    ###$data->{'doc'}->{'terms'}->[$position]=$raw_frequency;
+    $line.=$position.':'.$adjusted_frequency.' ';
+  }
+  chop $line;
+  delete $data->{'doc'}->{'position'};
+  ##print Dumper $data->{'doc'};
+  ##print "$line\n";
+  push(@{$data->{'ds'}},$line);
+  return $data;
+}
+
+
+
+# cheers!
 1;
 
--- lib/ACIS/Resources/Learn/Refused.pm
+++ lib/ACIS/Resources/Learn/Refused.pm
@@ -2,25 +2,17 @@ package ACIS::Resources::Learn::Refused;
 
 use strict;
 use warnings;
-use AI::Categorizer::Learner::NaiveBayes;
-use AI::Categorizer::Document;
-use AI::Categorizer::KnowledgeSet;
-use AI::Categorizer;
-
-#use Lingua::StopWords;
-#use AI::Categorizer::Learner::SVM;
-#use AI::Categorizer::Learner::KNN;
-#use AI::Categorizer::Learner::DecisionTree;
-#use AI::Categorizer::Learner::Guesser;
 use Data::Dumper;
 use Carp::Assert;
 use sql_helper;
 use ACIS::Web;
 use Web::App::Common;
 use Storable;
+use ACIS::Resources::Learn;
 
 require ACIS::APU;
 require ACIS::APU::Queue;
+
 
 #####  MAIN PART  
 ##my $homedir='/home/awho/acis';
@@ -52,13 +44,13 @@ sub sort_refused {
     &log("no login for $to_do");
     return;
   }
-  &log("founnd login: $login");
+  &log("found login: $login");
 
   require ACIS::Web::Admin;
   my $apply_learning = sub { 
 
     my $app = $_[0];
-
+    
     my $record = $app -> session -> current_record;
     if(not $record) {
       &log('no record');
@@ -66,21 +58,29 @@ sub sort_refused {
     my $psid = $record -> {'sid'};
     my $refused  = $record ->{'contributions'} -> {'refused'};
     if(not $refused ) {
+      &log("no refused contributions");
       return 1;
     }
     my $accepted  = $record ->{'contributions'} -> {'accepted'};
     if(not $accepted ) {
+      &log("no accepted contributions");
       return 1;
     }
-    &learn_all_refused($refused,$accepted,$psid) ;
+    my $refused_sorted=&learn_all_refused($accepted,$refused,$psid);
+    if(ref($refused_sorted)) {
+      my $out=Dumper $refused_sorted;
+      &log("$out");
+      $refused=$refused_sorted;
+    }
+    else {
+      &log("learn all refused returned false");
+    }
     return 1;
   };
-  
   my $res;
   eval {
     #  get hands on the userdata (if possible),
-    #  create a session and then do the work
-    
+    #  create a session and then do the work    
     $res = ACIS::Web::Admin::offline_userdata_service( $acis, $login, $apply_learning) || 'FAIL';    
     &log($@);
     &log($res);
@@ -92,156 +92,21 @@ sub sort_refused {
 # wrapper to test all refused documents
 #
 sub learn_all_refused {
-  my $refused=shift;
   my $accepted=shift;
+  my $refused=shift;
   # person short id, only used for logging purposes
   my $psid=shift;
-  clear_undefined $refused;
-  clear_undefined $accepted;
-  my @refused_docs=@{$refused};
-  my $count_refused=$#refused_docs;
-  @refused_docs=();
-  my @accepted_docs=@{$accepted};
-  my $count_accepted=$#accepted_docs;
-  @accepted_docs=();
-  # make sure it's worth proceeding
-  if($count_refused < 1 and $count_accepted > 0) {
+  &log(Dumper $refused);
+  # call the main learning
+  my $suggested=&ACIS::Resources::Learn::learn_via_svm($accepted,$refused);
+  if(not defined($suggested)) {
+    &log("learn_via_svm returned false");
     return;
   }
-  my $test_count;
-  my $result;
-  my $data;
-  foreach my $count_to_learn (0..$count_refused) {
-    my $score=&learn_one_document($refused,$accepted,$count_to_learn);
-    &log("$psid doc #$count_to_learn scores $score");
-      push (@{$result->{$score}},$count_to_learn);
+  if(not ref($suggested)) {
+    &log($suggested)
   }
-  my $docs_sorted;
-  my $docs_count;
-  &log("$psid start sorting");
-  foreach my $number (sort {$b <=> $a} keys %{$result}) {
-    foreach my $doc_number (@{$result->{$number}}) {
-      push(@{$docs_sorted},$refused->[$doc_number]);
-    }    
-  }
-  &log("$psid end sorting");
-  #print Dumper $docs_sorted;  
-  for(my $count=0; $count < $count_refused; $count++) {
-    $refused->[$count]=$docs_sorted->[$count];
-  }
-  my $out=Dumper $refused;
-  &log($out);
-  clear_undefined $refused;
-}
-
-#
-# finds the score of one document
-#
-sub learn_one_document {
-  my $refused=shift;
-  my $accepted=shift;
-  my $number=shift;
-  my @accepted_docs=@{$accepted};
-  my @refused_docs=@{$refused};
-  my $accepted_docs=\@accepted_docs;
-  my $test_doc=splice(@refused_docs,$number,1);
-  my $test_sid=$test_doc->{'sid'};
-  if(not $test_sid) {
-    return 0;
-  }
-  my $refused_docs=\@refused_docs;
-  my $k;
-  eval {
-    $k=new AI::Categorizer::KnowledgeSet();
-  };
-  &log($@);
-  #print Dumper $k;
-  #print "form documnents...\n";
-  &form_documents($accepted_docs,'accepted',$k);
-  #print "form documnents...\n";
-  &form_documents($refused_docs,'refused',$k);
-  my $nb = new AI::Categorizer::Learner::NaiveBayes();
-  #print "train\n";
-  $nb->train(knowledge_set => $k);
-  #print "trained\n";
-  #print Dumper $test_doc;
-  my $test=form_document($test_doc);
-  my $hypothesis;
-  eval {
-    $hypothesis=$nb->categorize($test);
-  } ;
-  &log($@);
-  my $score = $hypothesis->scores('accepted');
-  $k={};
-  undef($k);
-  $nb={};
-  undef($nb);
-  return $score;
-}
-
-
-sub form_documents {
-  my $in=shift;
-  my $state=shift;
-  my $k=shift;
-  my $my_category =  eval {
-    AI::Categorizer::Category->new(name => $state);
-  };
-  &log($@);
-  my $category=[$my_category];
-  foreach my $doc (@{$in}) {
-    if(not defined($doc->{'sid'})) {
-      next;
-    }
-    my $d=form_document($doc,$category) ;
-    eval {
-      $k->add_document($d);
-    };
-    &log($@);
-    $d={};
-    undef($d);
-  }
-}
-  
-
-sub form_document {
-  my $doc=shift ;
-  my $category=shift or undef;
-  my $sid=$doc->{'sid'};
-  #print "sid is $sid\n";
-  # Simplest way to create a document:
-  my $authors= lc($doc->{'authors'});
-  my $title= lc($doc->{'title'});
-  my $d;
-  if($category) {
-    eval {
-      $d = new AI::Categorizer::Document(name => $sid,
-                                         categories => $category,
-                                         content => { authors => $authors,
-                                                      title => $title},
-                                         content_weights => { authors => 1,
-                                                              title => 1}
-                                         #stopwords => Lingua::StopWords::getStopWords('en'),
-                                         #stemming => 'porter',
-                                        );
-    };    
-    &log($@);
-  }
-  else {
-    # leave out the category
-    eval {
-      $d = new AI::Categorizer::Document(name => $sid,
-                                         content => { authors => $authors,
-                                                      title => $title},
-                                         content_weights => { authors => 1,
-                                                              title => 1}
-                                         #stopwords => Lingua::StopWords::getStopWords('en'),
-                                         #stemming => 'porter',
-                                        );
-    };    
-    &log($@);
-  }
-  return $d;
+  return $suggested;
 }
 
 
