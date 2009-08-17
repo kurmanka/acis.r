61a925d54f4dc48e13f6c916c3a2045dfc3144fd
ACIS <acis@openlib.org>
2009-08-17 06:52:49 -0400

    learning software 2008-08-17
    darcs-hash:20090817105249-854f8-d7f9343c03783c22830c55693d8f8c2eed6b7d28.gz

--- lib/ACIS/Resources/Learn.pm
+++ lib/ACIS/Resources/Learn.pm
@@ -3,7 +3,7 @@ package ACIS::Resources::Learn;
 
 use Exporter;
 use base qw(Exporter);
-use vars qw(@EXPORT);
+use vars qw(@EXPORT @EXPORT_OK);
 use File::Temp ();
 use Web::App::Common;
 use ACIS::Web::Background qw(logit);
@@ -18,47 +18,98 @@ use strict;
 my $train_bin="/usr/bin/svm-train -q -b 1";
 my $predict_bin="/usr/bin/svm-predict -b 1";
 
-@EXPORT = qw( learn_via_svm() );
+@EXPORT_OK = qw( learn_via_svm form_learner);
 
 
-#
-# general learning function, 
-# takes accepetd, refused, suggested
-#
+## RESTART daemon when changing iths file
+
+##
+## general learning function, takes a learner
+## and changes relevance key is the elemnet to
+## learn about. no sorting. 
 sub learn_via_svm {
-  my $accepted=shift;
-  my $refused=shift;
-  # may be left out, then we learn about the 
-  # refused documents
-  my $suggested=shift;
-  my @accepted_docs=@{$accepted};
-  my $count_accepted=$#accepted_docs;
-  # there should be at least one accepted documet
-  if($count_accepted<0) {
-    return "no accepted documents";
+  my $learner=shift;
+  my $what_to_learn=shift;
+  my $debug=shift;
+  ## switch on debuggin
+  if(not defined($debug)) {
+    $debug=0;
   }
-  # save memory
-  @accepted_docs=();
-  ##print "count_accepted is $count_accepted\n";
-  my @refused_docs=@{$refused};
-  my $count_refused=$#refused_docs;
-  ##print "count_refused is $count_refused\n";
-  my @suggested_docs;
+  $debug=undef;
+  ##for debugging, save train file, save time
+  my $time=time();
+  if($debug) {
+    open(DEBUGLOG,"> /tmp/$time.learn");
+  }
+  my $accepted=$learner->{'accepted'};
+  if(not ref($accepted)) {
+    return "Error: \$accepted is not a reference.";
+  }
+  my $count_accepted=scalar @$accepted;
+  if(not $count_accepted) {
+    return "There are no accepted items, therefore no learning.";
+  }
+  if($debug) {
+    print DEBUGLOG "count_accepted is $count_accepted.\n";
+  }
+  my $refused=$learner->{'refused'};
+  if(not ref($refused)) {
+    return "Error: \$refused is not a reference.";
+  }
+  my $count_refused=scalar @$refused;
+  if(not $count_refused) {
+    return "There are no refused items, therefore no learning.";
+  }
+  if($debug) {
+    print DEBUGLOG "count_refused is $count_refused\n";
+  }
+  my $suggested=$learner->{'suggested'};
+  ## the variable above may not be defined
+  ## when the caller is known_items
   my $count_suggested;
   if(defined($suggested)) {
-    @suggested_docs=@{$suggested};
-    $count_suggested=$#suggested_docs;
-    @refused_docs=();
-  } 
+    $count_suggested=scalar @$suggested;
+  }
   else {
-    @suggested_docs=@{$refused};
-    $count_suggested=$#refused_docs;
+    $count_suggested=0;
   }
-  # make sure it's worth proceeding
-  if($count_suggested < 1) {
-    return "not enough suggested documents";
+  if($debug) {
+    print DEBUGLOG "count_suggested is $count_suggested\n";
   }
-  # build the dataset
+  ## the target of learning
+  my $target;
+  ## how many elements in the target
+  my $count_target;
+  ## indicator of what the start of the line to learn is
+  my $start_of_line_to_learn;
+  if($what_to_learn eq 'suggested') {
+    $target=$suggested;
+    $count_target=$count_suggested;
+    $start_of_line_to_learn='0';
+    if($count_suggested < 2) {
+      return "$count_suggested is not enough suggested items to learn them";
+    }
+  }
+  elsif($what_to_learn eq 'refused') {
+    $target=$refused;
+    $count_target=$count_refused;
+    $start_of_line_to_learn='-1';
+    if($count_refused < 2) {
+      return "$count_refused is not enough suggested items to learn them";
+    }
+  }
+  elsif($what_to_learn eq 'accepted') {
+    $target=$accepted;
+    $start_of_line_to_learn='+1';
+    $count_target=$count_accepted;
+    if($count_accepted < 2) {
+      return "$count_accepted is not enough accepted items to learn them";
+    }
+  }
+  else {
+    return "invalid option for what to learn: $what_to_learn";
+  }
+  ## build the dataset
   my $data;
   foreach my $doc (@{$accepted}) {
     $data=&add_document_to_data($data,$doc,'+1');
@@ -66,119 +117,98 @@ sub learn_via_svm {
   foreach my $doc (@{$refused}) {
     $data=&add_document_to_data($data,$doc,'-1');
   }
-  if($suggested) {
-    foreach my $doc (@{$suggested}) {
-      $data=&add_document_to_data($data,$doc,'0');
-    }
+  foreach my $doc (@{$suggested}) {
+    $data=&add_document_to_data($data,$doc,'0');
   }
-  # now the document data lines are in $data->{'ds'}
-  my @ds=@{$data->{'ds'}};
-  # start with the trainingfile
+  ## now the document data lines are in $data->{'ds'}
+  my @ds=@{$data->{'ds'}}; 
+  ## training data for accepted and refused documnet
   my $train_fh = File::Temp->new();
   my $train_file=$train_fh->filename;
-  # write the training set, leaving out docs
-  # that start with "0"
+  ## the model file stores the results of training
+  my $model_fh=File::Temp->new();
+  my $model_file=$model_fh->filename();
+  ## the test file contains the documents we want 
+  ## to learn about
+  my $test_fh = File::Temp->new();
+  my $test_file=$test_fh->filename; 
+  ## create out file, it contains the results
+  my $out_fh = File::Temp->new();    
+  my $out_file=$out_fh->filename();
+  ## write the training and testing files
   foreach my $doc_line (@ds) {
+    ## training file, with '+1' and '-1' lines
     if(not $doc_line=~m|^0|) {
       print $train_fh "$doc_line\n";
     }
+    ## the \Q and \E are needed for the '+1'
+    if($doc_line=~m|^\Q$start_of_line_to_learn\E|) {
+      ## replace the +1 or -1 with 0
+      ## the indication will probably be ignored but
+      ## it is nice to be precise.
+      $doc_line=~s|^\Q$start_of_line_to_learn\E|0|);
+      print $test_fh "$doc_line\n";
+    }
   }
   $train_fh->close;
-  # create model file
-  my $model_fh=File::Temp->new();
-  my $model_file=$model_fh->filename();
-  # create system command to fire up training
+  $test_fh->close;
+  ## create system command to fire up training
   my $s="$train_bin ".$train_fh->filename;
   $s.=" $model_file";
-  # for debugging, save train file
-  ##$s.="; cp $train_file /tmp/train";
-  ##print "doing $s\n";
+  ## for debuing, keep a copy of the intermediate files
+  if(defined($debug)) {    
+    $s.="; cp $train_file /tmp/$time.$what_to_learn.train";
+    $s.="; cp $model_file /tmp/$time.$what_to_learn.model";
+  }
   system($s);
-  # model is now trained, build the testing set
-  # organize test file
-  my $test_fh = File::Temp->new();
-  my $test_file=$test_fh->filename; 
-  # find the start of the suggested document
-  my $learn_doc_start;
-  if($suggested) {
-    $learn_doc_start=$count_accepted+$count_refused+1;
-  }
-  # if there are no suggested, start with the refused docs
-  else {
-    $learn_doc_start=$count_accepted+1
-  }  
-  foreach my $count_to_learn (0..$count_suggested) {
-    my $learn_doc=$data->{'ds'}->[$learn_doc_start+$count_to_learn];
-    # in case it was a refused document
-    $learn_doc=~s|^-1|0|;
-    ##print "wrote testing set line $count\n";
-    print $test_fh "$learn_doc\n";
-  }
-  $test_fh->close();
-  # organize out file
-  my $out_fh = File::Temp->new();    
-  my $out_file=$out_fh->filename();
-  # generate and read output file
+  ## model is now trained, build the testing set
   $s="$predict_bin $test_file $model_file $out_file";
-  # for debuing
-  ##$s.="; cp $out_file /tmp/out";
-  ##$s.="; cp $test_file /tmp/test"; 
-  # for the output, REQUIRED
+  ## for debuing, keep a copy of the intermediate files
+  if(defined($debug)) {
+    $s.="; cp $out_file /tmp/$time.$what_to_learn.out";
+    $s.="; cp $test_file /tmp/$time.$what_to_learn.test"; 
+  }
+  ## for the output, REQUIRED
   $s.="; cat $out_file";
-  ##print "doing: $s\n";
+  #print "doing: $s\n";
   my $count_to_learn=0;
-  # read output and find the score for earch document
+  ## read output and find the score for earch document
   my $result;
   foreach my $line (`$s`) {
-    ##print "$line";
     if(not $line=~m|^[+-]*1|) {
       next;
     }
     my @data=split(' ',$line);
     my $score=$data[1];
-    ##print "score is $score\n";
-    push(@{$result->{$score}},$count_to_learn);
+    ## add the score to the target
+    $target->[$count_to_learn]->{'relevance'}=$score;
     $count_to_learn++;
-  }  
-  my $docs_sorted;
-  my $docs_count;
-  # if there are no suggested documents, from here onwards consider
-  # the refused documents to be the suggested ones
-  if(not defined($suggested)) {
-    $suggested=$refused;
-  }
-  foreach my $number (sort {$b <=> $a} keys %{$result}) {
-    foreach my $doc_number (@{$result->{$number}}) {
-      push(@{$docs_sorted},$suggested->[$doc_number]);
-      if(not defined($suggested->[$doc_number])) {
-        print "fatal: document $doc_number is not defined!\n";
-        exit;
-      }
+  } 
+  ## check that we have got all documents covered
+  if(not $count_to_learn == $count_target) {
+    my $error="count_to_learn is $count_to_learn";
+    $error.=" but count_target is $count_target";
+    if($debug) {
+      print DEBUGLOG $error;
     }
+    return "$error";
   }
-  for(my $count=0; $count <= $count_suggested; $count++) {
-    if(not defined($docs_sorted->[$count])) {
-      print "fatal: docment $count is not defined!\n";
-      exit;
-    }
-    $suggested->[$count]=$docs_sorted->[$count];
+  if($debug) {
+    print DEBUGLOG "done\n";
   }
-  ##my $out=Dumper $suggested;
-  ##print "$out" ;
-  return $suggested;
+  return $target;
 }
 
-#
-# creates a line in the document
-#
+## creates a line in the document dataset
+## each line represents a documnet in svm format
 sub add_document_to_data {
   my $data=shift;
   my $doc=shift;
   my $label=shift;
-  # form terms to look at
+  ## form terms to look at
   my @terms=split('\W',$doc->{'authors'});
   push(@terms,split('\W',$doc->{'title'}));
-  # empty $data for this document;
+  ## empty $data for this document;
   #print "empty\n";
   $data->{'doc'}={};
   delete $data->{'doc'}->{'terms'};
@@ -188,10 +218,8 @@ sub add_document_to_data {
       next;
     }
     $term=lc($term);
-    #print "term is '$term'\n";
-    # new term
     my $term_number;
-    # new term 
+    ## new term 
     if(not $data->{'terms'}->{'known'}->{$term}) {
       $term_number=$data->{'terms'}->{'total_number'}++;
       #print "term number is $term_number\n";
@@ -201,32 +229,130 @@ sub add_document_to_data {
     else {
       $term_number=$data->{'terms'}->{'number'}->{$term};
     }
-    # count occurances of term
+    ## count occurances of term
     $data->{'doc'}->{'terms'}->[$term_number]++;
-    # keep track which term is actually used
+    ## keep track which term is actually used
     $data->{'doc'}->{'position'}->{$term_number}=$term;
-    # length, as measure by terms, of the document
+    ## length, as measure by terms, of the document
     $data->{'doc'}->{'total'}++;
   }
-  # now calculate weights such as the euclidean sum is one
+  ## now calculate weights such as the euclidean sum is one
   my $line="$label ";
   foreach my $position (sort {$a <=> $b} keys %{$data->{'doc'}->{'position'}}) {
     my $raw_frequency=$data->{'doc'}->{'terms'}->[$position];
     my $total=$data->{'doc'}->{'total'};
     my $adjusted_frequency=sqrt($raw_frequency/$total);
     $data->{'doc'}->{'terms'}->[$position]=$adjusted_frequency;
-    ###$data->{'doc'}->{'terms'}->[$position]=$raw_frequency;
+    #$data->{'doc'}->{'terms'}->[$position]=$raw_frequency;
     $line.=$position.':'.$adjusted_frequency.' ';
   }
   chop $line;
   delete $data->{'doc'}->{'position'};
-  ##print Dumper $data->{'doc'};
-  ##print "$line\n";
+  #print Dumper $data->{'doc'};
+  #print "$line\n";
   push(@{$data->{'ds'}},$line);
   return $data;
 }
-
-
+##
+## return a learner from an $app. called in modules that require learning
+## like send_suggestions_to_learning_daemon
+sub form_learner {
+  my $app=shift;
+  ## the name of the function that calls this function
+  my $origin=shift;
+  if(not defined($origin)) {
+    $origin='unknown origin';
+  }
+  ## if a third argumnt is given, it has results to learn
+  ## these results are added to the suggestions
+  my $results=shift;
+  ## a debug flag
+  my $debug=1;
+  ## gather variables
+  my $session = $app -> session;
+  my $vars    = $app -> variables;
+  my $record  = $session -> current_record;
+  my $id      = $record -> {'id'};
+  my $psid    = $record -> {'sid'};
+  ## the session id, only required for reporting
+  my $sid     = $session -> {'id'};
+  my $contributions;
+  ## daemon calls
+  if(ref($app->{'variables'}->{'contributions'})) {
+    $contributions=$app->{'variables'}->{'contributions'};
+  }
+  ## for offline calls
+  elsif(ref($record->{'contributions'})) {
+    $contributions=$record->{'contributions'};
+  }
+  else {
+    return "I can not get my hands on the contributions\n";
+  }
+  my $accepted=$contributions->{'accepted'};
+  my $refused=$contributions->{'refused'};
+  ## the suggestions
+  my $suggested;
+  if(ref($contributions->{'suggest'})) {
+    ## suggestions are grouped by reasons
+    my @groups=@{$contributions->{'suggest'}};
+    my $suggested_count=0;
+    ## suggestions united with no respect for reason 
+    foreach my $group (@groups) {
+      my $reason=$group->{'reason'};
+      foreach my $suggestion (@{$group->{'list'}}) {
+        ## add the reason to each suggested so that we don't loose it      
+        $suggestion->{'reason'}=$reason;
+        $suggested->[$suggested_count]=$suggestion;
+        $suggested_count++;
+      }
+    }
+  }
+  elsif(ref($contributions->{'suggested'})) {
+    ## probably pointless since this sholud only be 
+    ## true of known_item offline calls
+    $suggested=$contributions->{'suggested'};
+  }
+  else {
+    $suggested=[];
+  }
+  ## if there are $results, they have to be added
+  ## to the other suggested documents
+  if(ref($results)) {
+    ## merge the the results into $suggested by sid
+    ## first prepare a hash of handle if $suggestde
+    my $suggested_handles;
+    foreach my $suggestion (@{$suggested}) {
+      $suggested_handles->{$suggestion->{'dsid'}}=1;
+    }
+    ## then merge
+    foreach my $result (@{$results}) {
+      if(not defined($suggested_handles->{$result->{'dsid'}})) {
+        push(@{$suggested},$result);
+      }
+    }
+  }
+  ## the learner structure
+  my $learner;
+  $learner -> {'accepted'}   = $accepted;
+  $learner -> {'refused'}    = $refused;
+  $learner -> {'suggested'}  = $suggested;
+  $learner -> {'id'}         = $id;
+  $learner -> {'sid'}        = $sid;
+  $learner -> {'psid'}       = $psid;
+  $learner -> {'start_time'} = time();
+  ## this is only used by the learning of suggested
+  ## documents to form the log the sql object
+  $learner -> {'config'} = $app-> {'config'};
+  $learner -> {'origin'} = $origin;
+  ## for debugging, 
+  if($debug) {
+    open(DEBUGLOG,"> /tmp/".time().'.learner');
+    print DEBUGLOG Dumper $learner;
+    close DEBUGLOG;
+  }
+  ## return the learner!
+  return $learner;
+}
 
 # cheers!
 1;
