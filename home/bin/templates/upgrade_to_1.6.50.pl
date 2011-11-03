f81224cef4e9b55c162fd9b959cdd74034320b0c
Ivan Kurmanov <duraley@gmail.com>
2011-11-03 07:28:45 -0400

    new feature for ghost records tracking: timestamp column in the resources, objects and institutions tables
    Ignore-this: 645a5063d330c6a515374867b2bf445f
    
    darcs-hash:20111103112845-161e5-262f6cb7efc3ec57902861ab0df66c355ecb2212.gz

---
+++ home/bin/templates/upgrade_to_1.6.50.pl
@@ -0,0 +1,76 @@
+use strict;
+use warnings;
+
+#####  MAIN PART
+require ARDB;
+require ARDB::Local;
+
+my $ARDB = ARDB -> new() or die;
+my $sql = $ARDB -> sql_object;
+
+# Add the timestamp column to the resources, objects and institutions
+# tables. 
+#
+# It is first added with DEFAULT 0 to make sure the initial value that
+# is written into the existing records in the table is
+# "0000-00-00". When we later transform it to 'DEFAULT
+# CURRENT_TIMESTAMP', any subsequently inserted records would get the
+# current time (date) in the column.
+#
+# This column is basically managed by MySQL: it would automatically
+# keep the timestamp dates current on all stored records in the tables
+# that have this column. And then if a record does not get an update
+# in a while, we would find it by looking at this column's value. And
+# kill it.
+#
+# See also: home/bin/templates/sanitar_timestamp_based.pl
+# (bin/sanitar_timestamp_based.pl on an installed system).
+
+#  the SQL to run
+my @q = (
+    qq!
+alter table rdb.objects
+   add column timestamp TIMESTAMP default 0 on update CURRENT_TIMESTAMP !,
+    qq!
+alter table rdb.objects
+  modify column timestamp TIMESTAMP 
+    default CURRENT_TIMESTAMP 
+    on update CURRENT_TIMESTAMP !,
+
+    qq!
+alter table resources
+   add column timestamp TIMESTAMP default 0 on update CURRENT_TIMESTAMP !,
+    qq!
+alter table resources
+  modify column timestamp TIMESTAMP 
+    default CURRENT_TIMESTAMP 
+    on update CURRENT_TIMESTAMP !,
+
+    qq!
+alter table institutions
+   add column timestamp TIMESTAMP default 0 on update CURRENT_TIMESTAMP !,
+    qq!
+alter table institutions
+  modify column timestamp TIMESTAMP 
+    default CURRENT_TIMESTAMP 
+    on update CURRENT_TIMESTAMP !,
+
+    );
+
+print "please wait while we upgrade the database...\n";
+
+use Data::Dumper;
+foreach ( @q ) {
+  $sql -> prepare( $_ );
+  print "  $_\n";
+  my $r = $sql -> execute;
+  if ($r) {
+    print "  - result: modified ", $r->rows, " rows\n";
+  }
+  if ($sql->error) {
+    print "  - error: " , $sql->error, "\n";
+    last;
+  }
+}
+
+print "upgrade done.\n";
