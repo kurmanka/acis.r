45c223ea41721f051a1cfbdace3cc2db87288311
Ivan Kurmanov <duraley@gmail.com>
2011-11-13 06:44:29 -0500

    bin/restartwrapper.sh - a shell script wrapper for the fastcgi daemon
    Ignore-this: 5cdb945d98a0a669953edc935df182a2
    
    darcs-hash:20111113114429-161e5-3815c07ed9efb59ee46e1657e39b8f1f2ad46508.gz

---
+++ home/bin/restartwrapper.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# http://stackoverflow.com/questions/525247/how-do-i-daemonize-an-arbitrary-script-in-unix
+
+if [[ $# < 1 ]]; then
+    echo "Name of pid file not given."
+    exit
+fi
+
+# Get the pid file's name.
+PIDFILE=$1
+shift
+
+if [[ $# < 1 ]]; then
+    echo "No command given."
+    exit
+fi
+
+echo "Checking pid in file $PIDFILE."
+
+#Check to see if process running.
+PID=$(cat $PIDFILE 2>/dev/null)
+if [[ $? = 0 ]]; then
+    ps -p $PID >/dev/null 2>&1
+    if [[ $? = 0 ]]; then
+        echo "Command $1 already running."
+        exit
+    fi
+fi
+
+# Write our pid to file.
+echo $$ >$PIDFILE
+
+# Get command.
+COMMAND=$1
+shift
+
+# Run command until we're killed.
+while true; do
+    $COMMAND "$*"
+    sleep 10 # if command dies immediately, don't go into un-ctrl-c-able loop
+done
