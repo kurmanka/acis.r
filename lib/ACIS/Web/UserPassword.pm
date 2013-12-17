447167dce01e8318ddd2ae796df51f180b67cd6b
Ivan Kurmanov <duraley@gmail.com>
2013-12-17 14:17:10 +0300

    generating salt and hashing passwords; checking the password

--- lib/ACIS/Web/UserPassword.pm
+++ lib/ACIS/Web/UserPassword.pm
@@ -24,10 +24,14 @@ use strict;
 use warnings;
 
 # http://search.cpan.org/~mkanat/Math-Random-Secure-0.06/lib/Math/Random/Secure.pm
-use Math::Random::Secure; 
+#use Math::Random::Secure; 
+# http://search.cpan.org/~davido/Bytes-Random-Secure-0.28/lib/Bytes/Random/Secure.pm
+use Bytes::Random::Secure qw(random_bytes); 
+use MIME::Base64;
+use Encode;
 
 use Digest;
-use Digest::SHA2;
+use Digest::SHA qw( sha256_base64 );
 
 use Data::Dumper;
 use Carp::Assert;
@@ -36,26 +40,42 @@ use Web::App::Common qw( debug );
 use ACIS::Data::DumpXML qw(dump_xml);
 
 
-sub generate_random_string {
-  my $string;
-  # ...
-  return $string;
+sub generate_random_bytes {
+  return random_bytes( 32 );
+}
+
+sub generate_random_bytes_base64 {
+  my $bytes = generate_random_bytes();
+  my $string = encode_base64( $bytes );
+  return ($bytes, $string);
 }
 
 sub make_hash($$) {
   my $passw = shift;
   my $salt  = shift;
-  my $hash;
+  my $hash_string;
+
+  # prepend the salt
+  my $data = $salt . encode_utf8( $password );
+  # take sha256, in the base64 encoding
+  $hash_string = sha256_base64( $data );
   # ...
-  return $hash; 
+  return $hash_string; 
 }
 
 sub check_user_password {
-  my $app = shift;
-  my $password = shift;
-  my $session = $app->session or die;
-  my $result;
-  # ...
+  my $app      = shift;
+  my $password = shift || die;
+  my $session  = $app->session or die;
+  my $ud_owner = $session ->userdata_owner or die;
+  my $salt_b64 = $ud_owner->{password_salt_base64} or die;
+  my $salt     = decode_base64( $salt_b64 ) or die;
+
+  my $given_hash    = make_hash( $password, $salt ) or die;
+  my $existing_hash = $ud_owner->{password_hash_base64} or die;
+  # compare
+  my $result = ($given_hash eq $existing_hash);
+  # false or true
   return $result;
 }
 
@@ -63,18 +83,31 @@ sub generate_salt {
   my $app = shift;
   my $session = $app->session or die;
   my $ud_owner = $session ->userdata_owner or die;
-  my $salt;
-  # ...
-  # $ud_owner->{password_salt} = $salt;
-  return;
+  my ($salt, $salt_b64) = generate_random_bytes_base64();
+
+  # we store the base64 encoding of the salt
+  $ud_owner->{password_salt_base64} = $b64;
+  return $salt;
 }
 
 sub upgrade_clear_password {
   my $app = shift;
   my $session = $app->session or die;
   my $ud_owner = $session ->userdata_owner or die;
-  # ...
-  return 1;
+
+  if (exists $ud_owner->{password}) {
+    debug "clear password exsits";
+    my $salt = generate_salt( $app ) or die;
+    my $pass = delete $ud_owner->{password};
+
+    my $hash = make_hash( $password, $salt ) or die;
+    $ud_owner->{password_hash_base64} = $hash;
+    debug "password hash (b64): $hash";
+    
+    return 1;
+  }
+
+  return 0;
 }
 
 sub set_new_password {
@@ -91,13 +124,15 @@ sub create_password_reset {
   my $session = $app->session or die;
   my $ud_owner = $session ->userdata_owner or die;
   my $token;
-
+  # ...
   return $token;
 }
 
 sub check_reset_token {
   my $app = shift;
-
+  my $result;
+  # ...
+  return $result;
 }
 
 1;
